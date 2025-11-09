ba8b42f34714ba67e802b798d40be5abdcff91e2
RahafAlmarri <S2211113479@ku.edu.kw>
2025-11-09 15:45:43 +0300

    Rahaf

--- companyproj/compapp/views.py
+++ companyproj/compapp/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from .models import Employee
+from .models import Employee, Project
 
 # Create your views here.
 
 def employee_list(request):
     employees = Employee.objects.all().order_by('name')
+    return render(request, 'employee_list.html', {'employees': employees})
+
+def employee_detaill(request, employee_id):
+    employee = Employee.objects.get(id=employee_id)
+    projects = employee.project.all()
+    return render(request, 'employee_detail.html', {'employee': employee, 'projects': projects})
+
+def employee_engineers(request):
+    employees = Employee.objects.filter(position__icontains='engineer')
     return render(request, 'employee_list.html', {'employees': employees})
